1471084a63cdd354508c65749665f40c84342257
motokiti2011 <motokiti2011@gmail.com>
2024-07-15 14:30:40 +0900

    715 1

--- src/page/ProductMenu.tsx
+++ src/page/ProductMenu.tsx
@@ -1,9 +1,64 @@
+// import { useState, useEffect } from "react";
+import { generateClient } from 'aws-amplify/data'
+import type { Schema } from '../../amplify/data/resource'
+
+const client = generateClient<Schema>()
+
+export default function ProductMenu() {
+    const createTodo = async () => {
+        await client.models.Todo.create({
+          content: window.prompt("Todo content?"),
+          isDone: false
+        })
+      }
+    
+      return <div>
+        <button onClick={createTodo}>Add new todo</button>
+      </div>
+    }
+
+
+
+
+
+//     const [todos, setTodos] = useState<Schema["Todo"]["type"][]>([]);
+
+//     const fetchTodos = async () => {
+//       const { data: items, errors } = await client.models.Todo.list();
+//       setTodos(items);
+//     };
+  
+//     useEffect(() => {
+//       fetchTodos();
+//     }, []);
+  
+//     const createTodo = async () => {
+//       await client.models.Todo.create({
+//         content: window.prompt("Todo content?"),
+//         isDone: false,
+//       });
+  
+//       fetchTodos();
+//     }
+
+//   return <div>
+//     <h1>Product-menu</h1>
+//     <button onClick={createTodo}>Add new todo</button>
+//       <ul>
+//         {todos.map(({ id, content }) => (
+//           <li key={id}>{content}</li>
+//         ))}
+//       </ul>
+//   </div>
+// }
+
 // src/components/View.tsx
-const ProductMenu = () => {
-    return (
-        <>
-            <h1>Product-menu</h1>
-        </>
-    )
-} 
-export default ProductMenu;
+// const ProductMenu = () => {
+//     return (
+//         <>
+//             <h1>Product-menu</h1>
+//             <button onClick={createTodo}>Add new todo</button>
+//         </>
+//     )
+// } 
+// export default ProductMenu;
